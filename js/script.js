2fb66ecd76477486afd7a537d24a24edb9142ff3
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-14 14:38:24 +0100

    Add: strutture dati

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//add array titoli
+//add array paragrafi
+
+const titoliList = ['titolo1', 'titolo2', 'titolo3', 'titolo4']
+const paragrafiList = [ 'prova paragrafo uno',
+                        'prova paragrafo due',
+                        'prova paragrafo tre',
+                        'prova paragrafo quattro']
