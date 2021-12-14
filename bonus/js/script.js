ee9565a06de1b647075d1cfab028ae109a9b8e43
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-14 19:30:16 +0100

    Add: carousel img (transition non funzionante)

---
+++ bonus/js/script.js
@@ -0,0 +1,49 @@
+
+//add array immagini
+const imgList = ['./img/HEMIS_2506945.jpeg',
+                 './img/ocean.jpg',
+                 './img/R.jpg']
+
+//aggancio lo script agli elementi del DOM
+const boxImgHtml = document.querySelector('.box-img')
+const imgHtml = document.querySelector('img');
+const nextHtml = document.querySelector('.next');
+const previousHtml = document.querySelector('.previous');
+
+//creo un contatore da usare per scorrere gli elementi
+//degli array
+let currentIndex = 0;
+
+//riempio gli elementi Html con i contenuti di defoult
+imgHtml.src = imgList[currentIndex]
+
+//creo evento onclick sul div next dell'Html
+nextHtml.addEventListener('click', function () {
+    
+    //boxImgHtml.classList.remove('transizione')
+
+    //ad ogni click aumento il contatore e controllo se
+    //ha superato la lunghezze delle mie strutture dati
+    //se ha superato lo riporto a zero per far ripartire
+    //lo slider dall'inizio
+    currentIndex++;
+    if(currentIndex>imgList.length-1){
+        currentIndex=0;
+    }
+    imgHtml.src = imgList[currentIndex]
+
+    //boxImgHtml.classList.add('transizione')
+    
+})
+
+//creo evento onclick sul div previous dell'Html
+previousHtml.addEventListener('click', function () {
+
+    //funziona come il next ma al contrario
+    currentIndex--;
+    if(currentIndex<0){
+        currentIndex=imgList.length-1;
+    }
+    imgHtml.src = imgList[currentIndex]
+
+})
